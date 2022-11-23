ba19818551307957386f244413c7bfa740373dfc
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-11-23 18:58:06 +0300

    Question Slides almost done

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QVBoxLay
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
+rubrics = ["История Нового Года", "Снеговики", "Новогодние Блюда",
+           "Цитаты из фильмов", "Новый Год до Революции", "Dead Мороз", "Загадки", "Новый Год в Других Странах"]
+
 
 class App(QMainWindow):
     def __init__(self):
@@ -31,42 +34,34 @@ class App(QMainWindow):
 
     def read_csv(self, file_id, cost):
         with open(f"{file_id}.csv", 'r', encoding='utf-8') as file:
-            res = csv.reader(file, delimiter=';')
+            res = csv.reader(file, delimiter=';', quotechar='"')
             for i in res:
                 if i[0] == cost:
-                    self.open_question_window(i[1], i[2])
+                    self.window = QuestionWindow(
+                        self, q=i[1], a=i[2], p=i[0], r=rubrics[file_id])
+                    self.window.show()
                     # print(i)
-
-    def open_question_window(self, question, answer):
-        self.window = QuestionWindow(self, q=question, a=answer)
-        self.window.show()
 
 
 class QuestionWindow(QWidget):
-    def __init__(self, *args, q="q-Error", a='a-Error'):
-        print(args[0])
+    def __init__(self, *args, q="q-Error", a='a-Error', p='Error', r="Error"):
         super().__init__()
-        self.initUI(q, a)
-
-    def initUI(self, q, a):
+        uic.loadUi("question.ui", self)
         self.setGeometry(300, 300, 1513, 710)
         self.setWindowTitle('Вопрос')
-
-        self.verticalLayout = QVBoxLayout()
+        print(p, r, q, a)
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.setWindowFlag(Qt.FramelessWindowHint)
 
-        self.bg_label = QLabel(self)
-        self.bg_label.setText("lol")
-        self.bg_label.move(0, 0)
-        self.bg_label.setFixedSize(1513, 710)
-        self.bg_label.setPixmap(QPixmap("bg.png"))
+        self.label_3.setText(
+            f"""<html><head/><body><p align="center"><span style=" font-size:26pt; color:#5e9ad7;">{q}</span></p></body></html>""")
 
-        self.q_label = QLabel(self)
-        self.q_label.setText(q)
-        self.q_label.setWordWrap(True)
-        self.q_label.move(370, 320)
-        self.q_label.setFixedSize(500, 300)
+        real_rubric = """<html><head/><body><p align="center"><span style=" font-size:28pt; color:#e88a2c;">""" + \
+            r + " " + p + """</span></p></body></html>"""
+
+        self.label_2.setText(real_rubric)
+
+        self.pushButton.clicked.connect(self.close)
 
 
 if __name__ == '__main__':
