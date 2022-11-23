dd80e050ae290bddf5702a888695c662c9491453
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-11-23 15:17:05 +0300

    Created File System

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+import csv
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -8,10 +8,23 @@ class App(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('design.ui', self)
-        self.pushButton.clicked.connect(self.bruh)
+        for i in self.buttonGroup.buttons():
+            i.clicked.connect(self.bruh)
 
     def bruh(self):
-        print(1)
+        cost = self.sender().text()
+        id = int(self.sender().objectName().split('_')[1])
+        # print(id, cost)
+        self.read_csv(id // 10, cost)
+
+    def read_csv(self, file_id, cost):
+
+        with open(f"{file_id}.csv", 'r', encoding='utf-8') as file:
+            res = csv.reader(file, delimiter=';')
+            for i in res:
+                if i[0] == cost:
+                    print(i)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
