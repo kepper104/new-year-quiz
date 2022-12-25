c00ff0cbd47fa4a68b174f3ed5216098713741e7
kepper104 <67190416+kepper104@users.noreply.github.com>
2022-12-25 20:52:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ from PyQt5.QtCore import Qt
 from threading import Thread
 from os import getcwd
 from os import path
-import matplotlib
+import matplotlib.pyplot as plt
 import pygame
 
 # import playsound
+team_count = 3
 
+cur_team = 1
+points = {}
+for i in range(1, team_count + 1):
+    points[i] = 0
+print(points)
 
 rubrics = ["История Нового Года", "Снеговики", "Новогодние Блюда",
            "Цитаты из фильмов", "Новый Год до Революции", "Dead Мороз", "Загадки", "Новый Год в Других Странах"]
@@ -25,18 +31,23 @@ if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 all_music = []
+
+
 def playMusic(file_name):
     sound = SoundPlayer(file_name)
     all_music.append(Thread(target=sound.play_sound, args=()))
     all_music[-1].start()
 
+
 def killAllMusic():
     for i in all_music:
         i.stop()
 
+
 class SoundPlayer():
     def __init__(self, file_name):
         self.name = file_name
+
     def play_sound(self):
 
         cwd = getcwd()
@@ -49,6 +60,8 @@ class SoundPlayer():
 
         while True:
             pygame.time.Clock().tick(10)
+
+
 class App(QMainWindow):
     def __init__(self):
         pygame.init()
@@ -57,14 +70,11 @@ class App(QMainWindow):
 
         # USEFUL STUFF
         # ------------------------------
-        self.team_count = 3
         use_bg_image = False
         show_window_borders = True
         # ------------------------------
 
-
         # playsound("Ra-ta-ta.mp3")
-        self.cur_team = 0
 
         if use_bg_image:
             self.bg_label = QLabel(self)
@@ -103,11 +113,14 @@ class App(QMainWindow):
             self.read_music(id)
         else:
             self.read_csv(id // 10, cost)
+
     def music_pause(self, music_name):
         x = self.geometry().x()
         y = self.geometry().y()
-        self.window = QuestionWindow(self, pos=(x, y), isMusic=True, music_name=music_name)
+        self.window = QuestionWindow(self, pos=(
+            x, y), isMusic=True, music_name=music_name)
         self.window.show()
+
     def read_csv(self, file_id, cost):
         playMusic("next.mp3")
         with open(f"{file_id}.csv", 'r', encoding='utf-8') as file:
@@ -122,6 +135,7 @@ class App(QMainWindow):
                     self.window.show()
                 # print(i)
     # def read_music(self, file_id):
+
 
 class QuestionWindow(QWidget):
     def __init__(self, *args, q="Музыкальная пауза", a='a-Error', p='', r="", pos=(0, 0), isMusic=False, music_name=''):
@@ -151,24 +165,69 @@ class QuestionWindow(QWidget):
             r + " " + p + """</span></p></body></html>"""
 
         self.label_2.setText(real_rubric)
-        print("help")
+
     def showAnswer(self):
         self.label_3.setText(
             f"""<html><head/><body><p align="center"><span style=" font-size:26pt; color:#5e9ad7;">{self.answer}</span></p></body></html>""")
 
     def keyPressEvent(self, event):
+        global cur_team, points
         print(event.key())
         if event.key() == 16777220:
             if not self.shownAns:
                 if self.isMusic:
                     playMusic(self.music_name)
                 else:
-                    print("showing")
                     self.showAnswer()
                 self.shownAns = True
 
             else:
                 self.close()
+        elif event.key() == 89:
+            points[cur_team + 1] += int(self.price)
+            plot_histogram((cur_team + 1, self.price))
+            cur_team += 1
+            cur_team //= 3
+        elif event.key() == 78:
+            plot_histogram()
+            cur_team += 1
+            cur_team //= 3
+
+
+# 89 Y
+# 78 N
+
+def plot_histogram(last_points=(0, 0)):
+    teams = list(points.keys())
+    team_points = list(points.values())
+
+    fig = plt.figure(figsize=(15, 10))
+    ax = fig.add_subplot(111)
+    ax.set_ylabel("Очки", fontsize=16)
+    ax.set_title("Результаты", fontsize=16)
+    colors = [f"C{i}" for i in range(len(teams))]
+
+    for i, team in enumerate(teams):
+        ax.bar(team, team_points[i], color=colors[i])
+        ax.text(team, team_points[i] - 20, team_points[i],
+                ha="center", color="black", fontsize=14)
+
+    if last_points != (0, 0):
+        ax.text(last_points[0], team_points[last_points[0] - 1] + 5, "+" + str(last_points[1]), ha="center",
+                color="black", fontsize=14)
+
+    plt.xticks(teams, ["Команда {}".format(i) for i in teams], fontsize=13)
+
+    plt.yticks(team_points, fontsize=14)
+    print("before show")
+    plt.show()
+    print("shown")
+    # plot_thread = Thread(target=plt.show, args=())
+    # plot_thread.start()
+    # plt.show()
+
+    print(points)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
